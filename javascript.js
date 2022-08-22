873aa3f6f8220b97ee41a62a49ff9e058460a735
Bryce Tank <brycetank10@gmail.com>
2022-08-22 15:51:13 -0400

    Create rainbow mode -After the grid is created, the user can now select 'rainbow mode' and whichever pixel block they hover over will change to a random background color

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
-const button=document.querySelector(".gridSizeButton")
+const gridSizeBtn=document.querySelector(".gridSizeButton")
+const rainbowMode=document.querySelector(".rainbowMode")
 const gameScreenContainer=document.querySelector(".gameScreenContainer")
 
 
-button.addEventListener('click', function createGrid(){
+
+gridSizeBtn.addEventListener('click', function createGrid(){
     for(i=0;i<10;i++){
-        const pixelRows=document.createElement('div')
-        pixelRows.classList.add('pixelRows')
-        gameScreenContainer.appendChild(pixelRows)
+        const pixelRowCreation=document.createElement('div')
+        pixelRowCreation.classList.add('pixelRows')
+        gameScreenContainer.appendChild(pixelRowCreation)
         for(j=0;j<10;j++){
-        console.log(5)
-        const pixelBlocks=document.createElement('div')
-        pixelBlocks.classList.add('pixelBlocks')
-        pixelBlocks.textContent=''
-        pixelRows.appendChild(pixelBlocks)
+            console.log(5)
+            const pixelBlockCreation=document.createElement('div')
+            pixelBlockCreation.classList.add('pixelBlocks')
+            pixelBlockCreation.textContent=''
+            pixelRowCreation.appendChild(pixelBlockCreation)
         }}
+        createRainbowMode()
 })
+
+
+function createRainbowMode(){
+    const pixelBlocks=document.querySelectorAll(".pixelBlocks")
+    rainbowMode.addEventListener('click', ()=>{
+        pixelBlocks.forEach((pixelBlock)=>{
+            pixelBlock.addEventListener('mouseover', ()=>{
+                let color3=Math.floor(Math.random()*255)
+                let color2=Math.floor(Math.random()*255)
+                let color1=Math.floor(Math.random()*255)
+                pixelBlock.style.cssText=`background-color: rgb(${color1},${color2},${color3});`
+        })})})}
+
