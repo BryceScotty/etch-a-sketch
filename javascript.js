cfb7f8367f933400da1921ddfd1435cba0310f38
Bryce Tank <brycetank10@gmail.com>
2022-08-25 17:48:34 -0400

    Create slider/pen color, reformat event listeners -Slider is now present and can slide from 1-100 outputting all values between -Pen color can choose any color in hexadecimal form -Created one function that has all the code of the others but much more compact. Due to the forEach function in the main function, I had to repeat several lines of code for every button click. Now, it's only written once. This also means that there is one event listener connected to a node list of all the buttons instead of individually connecting each button to an event listener

--- javascript.js
+++ javascript.js
@@ -1,47 +1,47 @@
 const gridSizeBtn=document.querySelector(".gridSizeButton")
-const rainbowMode=document.querySelector(".rainbowMode")
 const gameScreenContainer=document.querySelector(".gameScreenContainer")
-const shadeMode=document.querySelector('.shadeMode')
+const buttons=document.querySelectorAll(".button")
 
+const slider = document.getElementById("slider");
+const sliderOutput = document.querySelector(".sliderOutput");
+sliderOutput.innerHTML=slider.value
+slider.oninput=function(){
+  sliderOutput.innerHTML = slider.value;
+}
 
 let mode=''
+
 gridSizeBtn.addEventListener('click', function createGrid(){
-    for(i=0;i<10;i++){
+    gameScreenContainer.textContent=''    
+    for(i=0;i<sliderOutput.innerHTML;i++){
         const pixelRowCreation=document.createElement('div')
         pixelRowCreation.classList.add('pixelRows')
         gameScreenContainer.appendChild(pixelRowCreation)
-        for(j=0;j<10;j++){
-            console.log(5)
+        for(j=0;j<sliderOutput.innerHTML;j++){
             const pixelBlockCreation=document.createElement('div')
             pixelBlockCreation.classList.add('pixelBlocks')
-            pixelBlockCreation.textContent=''
             pixelBlockCreation.style.cssText='background-color: rgb(255,255,255)'
             pixelRowCreation.appendChild(pixelBlockCreation)
-        }}
-
+        }
+    }
+    entireGame()
 })
 
-
-
-
-function createRainbowMode(){
+function entireGame(){
+    for(const button of buttons){
+        button.addEventListener('click', ()=>{
+            mode=button.id
+        })}
     const pixelBlocks=document.querySelectorAll(".pixelBlocks")
-    mode='rainbowMode'
     pixelBlocks.forEach((pixelBlock)=>{
         pixelBlock.addEventListener('mouseover', ()=>{
-            if(mode==='rainbowMode'){
+            if(mode=='rainbow'){
                 let color3=Math.floor(Math.random()*255)
                 let color2=Math.floor(Math.random()*255)
                 let color1=Math.floor(Math.random()*255)
                 pixelBlock.style.cssText=`background-color: rgb(${color1},${color2},${color3});`
-    }})})}
-
-function createShadeMode(){
-    const pixelBlocks=document.querySelectorAll(".pixelBlocks")
-    mode='shadeMode'
-    pixelBlocks.forEach((pixelBlock)=>{
-        pixelBlock.addEventListener('mouseover', ()=>{
-            if(mode=='shadeMode'){
+            }
+            if(mode=='shade'){
                 let stringBackgroundColors=pixelBlock.style.backgroundColor.slice(4,-1)
                 const arrayRGB=stringBackgroundColors.split(",")
                 arrayRGB[0]=Number(arrayRGB[0])-25
@@ -49,21 +49,111 @@ function createShadeMode(){
                 arrayRGB[2]=Number(arrayRGB[2])-25
                 pixelBlock.style.cssText=`background-color: rgb(${arrayRGB})`
             }
-        })
-    })
-}
-rainbowMode.addEventListener('click', createRainbowMode)
-shadeMode.addEventListener('click', createShadeMode)
+            if(mode=='lighten'){
+                let stringBackgroundColors=pixelBlock.style.backgroundColor.slice(4,-1)
+                const arrayRGB=stringBackgroundColors.split(",")
+                arrayRGB[0]=Number(arrayRGB[0])+25
+                arrayRGB[1]=Number(arrayRGB[1])+25
+                arrayRGB[2]=Number(arrayRGB[2])+25
+                pixelBlock.style.cssText=`background-color: rgb(${arrayRGB})`
+            }
+            if(mode=='specificColorChooser'){
+                console.log('ay')
+                pixelBlock.style.cssText=`background-color: ${specificColorChooser.value}`
+            }
+        })})}
+
+    
 
 
-// const shadeMode=document.querySelector('.shadeMode')
-// shadeMode.addEventListener('click', () =>{
-//     console.log(shs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createRainbowMode(){
 //     const pixelBlocks=document.querySelectorAll(".pixelBlocks")
+//     mode='rainbowMode'
+//     console.log('heyy')
 //     pixelBlocks.forEach((pixelBlock)=>{
 //         pixelBlock.addEventListener('mouseover', ()=>{
-//             shs=pixelBlock.style.backgroundColor
-//             console.log(shs)
-//             pixelBlock.style.background='rgb(0,0,0)'
-//             console.log(pixelBlock.style.backgroundColor)
-//     })})})
+//             if(mode==='rainbowMode'){
+//                 let color3=Math.floor(Math.random()*255)
+//                 let color2=Math.floor(Math.random()*255)
+//                 let color1=Math.floor(Math.random()*255)
+//                 pixelBlock.style.cssText=`background-color: rgb(${color1},${color2},${color3});`
+//                 console.log(pixelBlock.style.backgroundColor)
+//                 console.log("------")
+                
+
+//     }})})}
+
+// function createShadeMode(){
+//     const pixelBlocks=document.querySelectorAll(".pixelBlocks")
+//     mode='shadeMode'
+//     pixelBlocks.forEach((pixelBlock)=>{
+//         pixelBlock.addEventListener('mouseover', ()=>{
+//             if(mode=='shadeMode'){
+//                 let stringBackgroundColors=pixelBlock.style.backgroundColor.slice(4,-1)
+//                 const arrayRGB=stringBackgroundColors.split(",")
+//                 arrayRGB[0]=Number(arrayRGB[0])-15
+//                 arrayRGB[1]=Number(arrayRGB[1])-15
+//                 arrayRGB[2]=Number(arrayRGB[2])-15
+//                 pixelBlock.style.cssText=`background-color: rgb(${arrayRGB})`
+//             }
+//         })
+//     })
+// }
+
+// function createLightenMode(){
+//     const pixelBlocks=document.querySelectorAll(".pixelBlocks")
+//     mode='lightenMode'
+//     pixelBlocks.forEach((pixelBlock)=>{
+//         pixelBlock.addEventListener('mouseover', ()=>{
+//             if(mode=='lightenMode'){
+//                 let stringBackgroundColors=pixelBlock.style.backgroundColor.slice(4,-1)
+//                 const arrayRGB=stringBackgroundColors.split(",")
+//                 arrayRGB[0]=Number(arrayRGB[0])+20
+//                 arrayRGB[1]=Number(arrayRGB[1])+20
+//                 arrayRGB[2]=Number(arrayRGB[2])+20
+//                 pixelBlock.style.cssText=`background-color: rgb(${arrayRGB})`
+//             }
+//         })
+//     })
+    
+// }
+
+// rainbowMode.addEventListener('click', createRainbowMode)
+// shadeMode.addEventListener('click', createShadeMode)
+// lightenMode.addEventListener('click', createLightenMode)
